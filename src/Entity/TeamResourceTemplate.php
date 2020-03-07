560bee4aefa483702b0491b9edfb33abe67c3473
Alex Dryden <adryden3@illinois.edu>
2020-03-07 13:05:33 -0500

    added TeamResourceTemplate entity because this is not mapped through resources in the core data model but is its own standalone table

---
+++ src/Entity/TeamResourceTemplate.php
@@ -0,0 +1,75 @@
+<?php
+
+
+namespace Teams\Entity;
+
+
+use Doctrine\ORM\Mapping\Id;
+use Doctrine\ORM\Mapping\JoinColumn;
+use Doctrine\ORM\Mapping\ManyToOne;
+use Omeka\Entity\ResourceTemplate;
+
+/**
+ * User is a table in the core, so it is not annotable, so the join table is
+ * declared as the entity TeamResource in order to bypass this issue.
+ * This entity is available only by the orm, not by Omeka S.
+ *
+ * @Entity
+ */
+class TeamResourceTemplate
+{
+
+    /**
+     * @var Team
+     * @Id
+     * @ManyToOne(
+     *     targetEntity="Teams\Entity\Team",
+     *     inversedBy="team_resources",
+     *     cascade={"persist"}
+     * )
+     * @JoinColumn(
+     *     onDelete="cascade",
+     *     nullable=false
+     * )
+     */
+    protected $team;
+
+    /**
+     * @var ResourceTemplate
+     * @Id
+     * @ManyToOne(
+     *     targetEntity="Omeka\Entity\ResourceTemplate",
+     *     cascade={"persist"}
+     * )
+     * @JoinColumn(
+     *     onDelete="cascade",
+     *     nullable=false
+     * )
+     */
+    protected $resource_template;
+
+    public function __construct(Team $team, ResourceTemplate $resource_template)
+    {
+        $this->team = $team;
+        $this->resource_template = $resource_template;
+    }
+
+    public function getTeam()
+    {
+        return $this->team;
+    }
+
+    public function getResourceTemplate()
+    {
+        return $this->resource_template;
+    }
+
+    public function __toString()
+    {
+        return json_encode([
+            'team' => $this->getTeam()->getId(),
+            'resource_template' => $this->getResourceTemplate()->getId(),
+        ]);
+    }
+
+}
