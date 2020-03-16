6c7b7f67a505fd6008aad55798558b427b2bc2d0
Alex Dryden <adryden3@illinois.edu>
2020-03-16 15:03:38 -0400

    changed the team detail view so it just shows a summary of total resources by resource type. Also figured out how to filter based on item type more effeciently and that unused code is commented out in the controller

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -77,6 +77,44 @@ class IndexController extends AbstractActionController
 
         return $view;
     }
+    public function teamResources($resource_type, $query, $user_id, $active = true, $team_id = null)
+    {
+        if ($team_id) {
+
+            $team_entity = $this->entityManager->getRepository('Teams\Entity\Team')->findOneBy(['id' => $team_id]);
+
+
+
+
+            $q = $this->entityManager->createQuery("SELECT resource FROM Omeka\Entity\Resource resource WHERE resource INSTANCE OF Omeka\Entity\Item");
+            $item_sets = $q->getArrayResult();
+            $team_resources = array();
+            foreach ($team_entity->getTeamResources() as $team_resource):
+                //obv here would be a place where you could just use the discriminator to see if it is an item
+                if (array_search($team_resource->getResource()->getId(), array_column($item_sets, 'id')) ){
+                    $team_resources[] = $team_resource;
+                }
+            endforeach;
+            $per_page = 10;
+            $page = $query['page'];
+            $start_i = ($per_page * $page) - $per_page;
+//            $tr = $team_entity->getTeamResources();
+            $max_i = count($team_resources);
+            if ($max_i < $start_i + $per_page){
+                $end_i = $max_i;
+            }else{$end_i = $start_i + $per_page;}
+//            $tr = $team_entity->getTeamResources();
+            for ($i = $start_i; $i < $end_i; $i++) {
+
+                $resources[] = $this->api()->read($resource_type, $team_resources[$i]->getResource()->getId())->getContent();}
+
+        }else{$team_resources=null;}
+
+        return array('page_resources'=>$resources, 'team_resources'=>$team_resources);
+
+
+
+    }
 
     public function teamDetailAction()
     {
@@ -84,38 +122,97 @@ class IndexController extends AbstractActionController
         $id = $this->params()->fromRoute('id');
         $response = $this->api()->read('team', ['id' => $id]);
         $team_entity = $this->entityManager->getRepository('Teams\Entity\Team')->findOneBy(['id'=>$id]);
-        $items_te = $team_entity->getResources();
-        $items = array();
+
+//        $items_te = $team_entity->getTeamResources()->getValues();
+//        $items_omeka = $this->entityManager->getRepository('Omkea\Entity\Item')->findAll();
+
+
+//        $resource_id = array_column(array_values($items_te),'resource');
+//        $view->setVariable('test', $items_te);
+//        $count = count($resource_id);
+//        $items = array();
         $em = $this->entityManager;
-        $qb = $em->createQueryBuilder();
-        $q = $qb->select('tr')
-            ->from('Teams\Entity\TeamResource', 'tr')
-            ->join('Omeka\Entity\Resource', 'r')
+//        $count = $em->createQueryBuilder();
+//        $count->select('count(distinct(tr.resource))')
+//            ->from('Teams\Entity\TeamResource', 'tr')
+//            ->leftJoin('Omeka\Entity\Resource', 'r')
 //            ->where('r INSTANCE of Omeka\Entity\Item')
-            ->where('tr.team = 5')
-        ;
-        $q = $this->entityManager->createQuery("SELECT resource FROM Teams\Entity\TeamResource resource WHERE resource.team = 6");
-        $team_resources = $q->getResult();
+//            ->where('tr.team = ?1');
+//        $count->setParameter(1, $this->params('id'));
+//        $count = $count->getQuery()->getSingleScalarResult();
+        $resources = [
+            'items'=> ['count' => 0, 'entity' => 'Item', 'team_entity' => 'TeamResource', 'fk' => 'resource'],
+            'item sets' => ['count' => 0, 'entity' => 'ItemSet', 'team_entity' => 'TeamResource', 'fk' => 'resource'],
+            'media' => ['count' => 0, 'entity' => 'Media', 'team_entity' => 'TeamResource', 'fk' => 'resource'],
+            'resource templates' => ['count' => 0, 'entity' => 'ResourceTemplate', 'team_entity' => 'TeamResourceTemplate', 'fk' => 'resource_template'],
+            'sites' => ['count' => 0, 'entity' => 'Site', 'team_entity' => 'TeamSite', 'fk' => 'site']
+        ];
 
-        $items = array();
-        $item_sets = array();
-        $media = array();
+        foreach ($resources as $key => $resource):
+        //I imagine this as like a subquery that gets the list of item ids
+            $sub_query = $em->createQueryBuilder();
+            $sub_query->select('r.id')
+                ->from('Omeka\Entity\\' . $resource['entity'], 'r');
 
+            $ids = $sub_query->getQuery()->getArrayResult();
 
-        foreach ($team_resources as $tr):
-            $tr = $tr->getResource();
-            if ($tr->getResourceName() == 'items') {
-                $items[] = $this->api()->read('items', $tr->getId())->getContent();
+            //get the count of the total number of team items
+            $qb = $em->createQueryBuilder();
 
-            } elseif ($tr->getResourceName() == 'item_sets'){
-                $item_sets[] = $this->api()->read('item_sets', $tr->getId())->getContent();
-            } elseif ($tr->getResourceName() == 'media'){
-                $media[] = $this->api()->read('media', $tr->getId())->getContent();
-            } else {}
+            $qb->select('count(tr.' . $resource['fk'] . ')')
+                ->from('Teams\Entity\\' . $resource['team_entity'], 'tr')
+                ->where('tr.team = ?1')
+                ->andWhere('tr.' . $resource['fk'] . ' in (:ids)')
+                ->setParameter('ids', $ids)
+            ;
+            $qb->setParameter(1, $this->params('id'));
+            $resources[$key]['count'] += $qb->getQuery()->getSingleScalarResult();
         endforeach;
-        $this->paginator(count($items), $this->params()->fromQuery('page'));
+
+        $view->setVariable('resources', $resources);
+
+//        //get the page resources for the page of results the user requested
+//        $qb = $em->createQueryBuilder();
+//        $q = $qb->select('tr')
+//            ->from('Teams\Entity\TeamResource', 'tr')
+//            ->where('tr.team = ?1')
+//            ->andWhere('tr.resource in (:ids)')
+//            ->setParameter('ids', $ids)
+//        ;
+//        //establish first and last results to get for the page
+//        //if a page is listed in the query, start there. Else, start at 0
+//        $items_per_page = 10;
+//        if ($this->params()->fromQuery()['page']){
+//            $offset = ($this->params()->fromQuery()['page']*$items_per_page)-$items_per_page;
+//
+//        }else{$offset=0;}
+//        $limit = $offset + $items_per_page;
+//
+//        $qb->setFirstResult($offset);
+//        $qb->setMaxResults($limit);
+//        $qb->setParameter(1, $this->params('id'));
+//
+//        $team_resources = $q->getQuery()->getResult();
+//
+//        $items = array();
+//        $item_sets = array();
+//        $media = array();
+//
+//
+//        foreach ($team_resources as $tr):
+//            $tr = $tr->getResource();
+//            if ($tr->getResourceName() == 'items') {
+//                $items[] = $this->api()->read('items', $tr->getId())->getContent();
+//
+//            } elseif ($tr->getResourceName() == 'item_sets'){
+//                $item_sets[] = $this->api()->read('item_sets', $tr->getId())->getContent();
+//            } elseif ($tr->getResourceName() == 'media'){
+//                $media[] = $this->api()->read('media', $tr->getId())->getContent();
+//            } else {}
+//        endforeach;
+//        $this->paginator($count, $this->params()->fromQuery('page'));
         $view->setVariable('response', $response);
-        $view->setVariable('items', $items);
+//        $view->setVariable('items', $items);
 
 
         return $view;
