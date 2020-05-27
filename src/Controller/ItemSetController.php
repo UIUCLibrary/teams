37c9b2f0024f362693b7763b36dc339182a55ae9
Alex Dryden <adryden3@illinois.edu>
2020-05-26 22:15:18 -0400

    clean up refactored code and merge with changes from 2.0

--- src/Controller/ItemSetController.php
+++ src/Controller/ItemSetController.php
@@ -92,7 +92,6 @@ class ItemSetController extends  \Omeka\Controller\Admin\ItemSetController
         if ($this->getRequest()->isPost()) {
             $data = $this->params()->fromPost();
             $data = $this->mergeValuesJson($data);
-
             $form->setData($data);
 
             $entity = $this->entityManager->getRepository('Teams\Entity\TeamResource')->findBy(['resource' => $this->params('id')]);
@@ -124,83 +123,11 @@ class ItemSetController extends  \Omeka\Controller\Admin\ItemSetController
         return $view;
     }
 
-
-    public function teamResources($resource_type, $query, $user_id, $active = true, $team_id = null)
-    {
-        if ($active){
-            $team_user = $this->entityManager->getRepository('Teams\Entity\TeamUser')->findOneBy(['user' => $user_id, 'is_current' => 1 ]);
-
-        } else{
-            $team_user = $this->entityManager->getRepository('Teams\Entity\TeamUser')->findOneBy(['user' => $user_id, 'team' => $team_id ]);
-        }
-
-        $resources = array();
-        if ($team_user) {
-
-            $active_team_id = $team_user->getTeam()->getId();
-
-            $team_entity = $this->entityManager->getRepository('Teams\Entity\Team')->findOneBy(['id' => $active_team_id]);
-
-
-
-
-            $q = $this->entityManager->createQuery("SELECT resource FROM Omeka\Entity\Resource resource WHERE resource INSTANCE OF Omeka\Entity\ItemSet");
-            $item_sets = $q->getArrayResult();
-            $team_resources = array();
-            foreach ($team_entity->getTeamResources() as $team_resource):
-                //obv here would be a place where you could just use the discriminator to see if it is an itemset
-                if (array_search($team_resource->getResource()->getId(), array_column($item_sets, 'id')) ){
-                    $team_resources[] = $team_resource;
-                }
-                endforeach;
-            $per_page = 10;
-            $page = $query['page'];
-            $start_i = ($per_page * $page) - $per_page;
-//            $tr = $team_entity->getTeamResources();
-            $max_i = count($team_resources);
-            if ($max_i < $start_i + $per_page){
-                $end_i = $max_i;
-            }else{$end_i = $start_i + $per_page;}
-//            $tr = $team_entity->getTeamResources();
-            for ($i = $start_i; $i < $end_i; $i++) {
-
-                $resources[] = $this->api()->read($resource_type, $team_resources[$i]->getResource()->getId())->getContent();}
-
-        }else{$team_resources=null;}
-
-        return array('page_resources'=>$resources, 'team_resources'=>$team_resources);
-
-
-
-    }
-    public function changeCurrentTeamAction($user_id)
-    {
-        $request = $this->getRequest();
-        if (!$request->isPost()) {
-            return $this->redirect()->toRoute('admin');
-        } else {
-            $data = $request->getPost();
-            $em = $this->entityManager;
-            $team_user = $em->getRepository('Teams\Entity\TeamUser');
-            $old_current = $team_user->findOneBy(['user' => $user_id, 'is_current' => true]);
-            $new_current = $team_user->findOneBy(['user'=> $user_id, 'team'=>$data['team_id']]);
-            if ($old_current){
-            $old_current->setCurrent(null);
-            //no idea why, but this function fails in this controller only unless the action is flushed first
-            $em->flush();}
-            $new_current->setCurrent(true);
-            $em->flush();
-
-
-        }
-    }
-
-
     public function browseAction()
     {
         $this->setBrowseDefaults('created');
         $response = $this->api()->search('item_sets', $this->params()->fromQuery());
-        $this->paginator($response->getTotalResults(), $this->params()->fromQuery('page'));
+        $this->paginator($response->getTotalResults());
 
         $formDeleteSelected = $this->getForm(ConfirmForm::class);
         $formDeleteSelected->setAttribute('action', $this->url()->fromRoute(null, ['action' => 'batch-delete'], true));
@@ -252,7 +179,7 @@ class ItemSetController extends  \Omeka\Controller\Admin\ItemSetController
     {
         $this->setBrowseDefaults('created');
         $response = $this->api()->search('item_sets', $this->params()->fromQuery());
-        $this->paginator($response->getTotalResults(), $this->params()->fromQuery('page'));
+        $this->paginator($response->getTotalResults());
 
         $view = new ViewModel;
         $view->setVariable('itemSets', $response->getContent());
@@ -365,11 +292,6 @@ class ItemSetController extends  \Omeka\Controller\Admin\ItemSetController
             return $this->redirect()->toRoute(null, ['action' => 'browse'], true);
         }
 
-        $resources = [];
-        foreach ($resourceIds as $resourceId) {
-            $resources[] = $this->api()->read('item_sets', $resourceId)->getContent();
-        }
-
         $form = $this->getForm(ResourceBatchUpdateForm::class, ['resource_type' => 'itemSet']);
         $form->setAttribute('id', 'batch-edit-item-set');
         if ($this->params()->fromPost('batch_update')) {
@@ -391,6 +313,11 @@ class ItemSetController extends  \Omeka\Controller\Admin\ItemSetController
             } else {
                 $this->messenger()->addFormErrors($form);
             }
+        }
+
+        $resources = [];
+        foreach ($resourceIds as $resourceId) {
+            $resources[] = $this->api()->read('item_sets', $resourceId)->getContent();
         }
 
         $view = new ViewModel;
