33c0e777cfd119bb5deca1daf793db04da66d97b
Alex Dryden <adryden3@illinois.edu>
2020-05-26 22:25:26 -0400

    clean up refactored code and merge with changes from 2.0

--- src/Controller/ResourceTemplateController.php
+++ src/Controller/ResourceTemplateController.php
@@ -33,80 +33,6 @@ class ResourceTemplateController extends \Omeka\Controller\Admin\ResourceTemplat
         $this->entityManager = $entityManager;
         $this->dataTypeManager = $dataTypeManager;
     }
-    public function teamResources($resource_type, $query, $user_id, $active = true, $team_id = null)
-    {
-
-        $team_user = $this->entityManager->getRepository('Teams\Entity\TeamUser')->findOneBy(['user' => $user_id, 'is_current' => 1 ]);
-        // TODO add if teamuser
-
-        $team = $team_user->getTeam();
-        $team_resource_templates = $team->getTeamResourceTemplates();
-
-        $team_rts = array();
-
-
-        foreach ($team_resource_templates as $rt):
-            $team_rts[] =  $this->api()->read('resource_templates', $rt->getResourceTemplate()->getId())->getContent();
-        endforeach;
-
-
-
-        //this logic looks at which page they requested, how many items per page, and collects the appropriate results.
-        $per_page = 10;
-
-            $page = $query['page'];
-            $start_i = ($per_page * $page) - $per_page;
-            $max_i = count($team_rts);
-            if ($max_i < $start_i + $per_page){
-                $end_i = $max_i;
-            }else{$end_i = $start_i + $per_page;}
-
-            $page_resources = array();
-            for ($i = $start_i; $i < $end_i; $i++) {
-
-                $page_resources[] = $team_rts[$i];
-            }
-
-
-        return array('page_resources'=>$page_resources, 'team_resources'=>$team_rts);
-
-
-
-    }
-    public function changeCurrentTeamAction($user_id)
-    {
-        $request = $this->getRequest();
-        if (!$request->isPost()) {
-            return $this->redirect()->toRoute('admin');
-        } else {
-            $data = $request->getPost();
-            $em = $this->entityManager;
-            $team_user = $em->getRepository('Teams\Entity\TeamUser');
-            $old_current = $team_user->findOneBy(['user' => $user_id, 'is_current' => true]);
-            $new_current = $team_user->findOneBy(['user'=> $user_id, 'team'=>$data['team_id']]);
-            if ($old_current){
-                $old_current->setCurrent(null);
-                //no idea why, but this function fails in this controller only unless the action is flushed first
-                $em->flush();}
-            $new_current->setCurrent(true);
-            $em->flush();
-
-
-        }
-    }
-
-
-    public function browseAction()
-    {
-        $this->setBrowseDefaults('owner_name', 'dec');
-        $response = $this->api()->search('resource_templates', $this->params()->fromQuery());
-        $this->paginator($response->getTotalResults(), $this->params()->fromQuery('page'));
-
-        $view = new ViewModel;
-        $view->setVariable('resourceTemplates', $response->getContent());
-        return $view;
-
-    }
 
     protected function getAddEditView()
     {
@@ -144,6 +70,12 @@ class ResourceTemplateController extends \Omeka\Controller\Admin\ResourceTemplat
             $data = $resourceTemplate->jsonSerialize();
             if ($data['o:resource_class']) {
                 $data['o:resource_class[o:id]'] = $data['o:resource_class']->id();
+            }
+            if ($data['o:title_property']) {
+                $data['o:title_property[o:id]'] = $data['o:title_property']->id();
+            }
+            if ($data['o:description_property']) {
+                $data['o:description_property[o:id]'] = $data['o:description_property']->id();
             }
             $form->setData($data);
         }
@@ -210,7 +142,6 @@ class ResourceTemplateController extends \Omeka\Controller\Admin\ResourceTemplat
                     }
                     $this->messenger()->addSuccess($successMessage);
                     return $this->redirect()->toUrl($response->getContent()->url());
-
                 }
             } else {
                 $this->messenger()->addFormErrors($form);
