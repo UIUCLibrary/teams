0d14d40b98dcec37f08b61aff82627945b819540
Alex Dryden <adryden3@illinois.edu>
2020-07-15 10:35:57 -0400

    remove items set element from form

--- src/Form/TeamRoleFieldset.php
+++ src/Form/TeamRoleFieldset.php
@@ -69,20 +69,20 @@ class TeamRoleFieldset extends Fieldset
             ]
         ]);
 
-        $this->add([
-            'name' => 'can_add_itemsets',
-            'type' => 'Checkbox',
-            'options' => [
-                'label' => '<strong class="o-icon-item-sets"> <strike>Can add itemsets to team repository</strike> Disabled</strong>',
-                'label_options' => [
-                    'disable_html_escape' => true,
-                ],
-            ],
-            'attributes' => [
-                'id' => 'can_add_itemsets',
-                'disabled' => 'disabled'
-            ]
-        ]);
+//        $this->add([
+//            'name' => 'can_add_itemsets',
+//            'type' => 'Checkbox',
+//            'options' => [
+//                'label' => '<strong class="o-icon-item-sets"> <strike>Can add itemsets to team repository</strike> Disabled</strong>',
+//                'label_options' => [
+//                    'disable_html_escape' => true,
+//                ],
+//            ],
+//            'attributes' => [
+//                'id' => 'can_add_itemsets',
+//                'disabled' => 'disabled'
+//            ]
+//        ]);
 
         $this->add([
             'name' => 'can_modify_resources',
