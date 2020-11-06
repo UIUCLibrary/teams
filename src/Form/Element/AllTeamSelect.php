ec847f56f474f58edb433b17f753c94a89e33eae
Alex Dryden <adryden3@illinois.edu>
2020-11-06 14:46:22 -0500

    add option to create new team for user

--- src/Form/Element/AllTeamSelect.php
+++ src/Form/Element/AllTeamSelect.php
@@ -18,11 +18,14 @@ class AllTeamSelect extends TeamSelect
         $teams = $em->getRepository('Teams\Entity\Team')->findAll();
         //this is set to display the teams for the current user. This works in many contexts for
         //normal users, but not for admins doing maintenance or adding new users to a team
+        $valueOptions[0] = '~~Add New Team~~';
         foreach ($teams as $team):
             $team_name = $team->getName();
             $team_id = $team->getId();
             $valueOptions[$team_id] = $team_name;
         endforeach;
+
+
 
 
         $prependValueOptions = $this->getOption('prepend_value_options');
