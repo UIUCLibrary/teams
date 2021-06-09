cab0c3206c11c66e2c2bc8cd7af09c710acb5b99
Alex Dryden <adryden3@illinois.edu>
2021-06-09 08:43:53 -0400

    if user chooses teams for default sites, disable the manual entry field

--- asset/js/user-default-sites.js
+++ asset/js/user-default-sites.js
@@ -1,3 +1,21 @@
+window.addEventListener("load", function () {
+    $("#update_default_sites").click(function() {
+        if($(this).is(":checked"))
+        {
+            $("#default_sites_chosen").attr("hidden",true);
+        }
+        else
+        {
+            $("#default_sites_chosen").attr("hidden",false);
+
+        }
+    });
+});
+
+
+
+
+
 $("#update_default_sites").click(function() {
     if($(this).is(":checked"))
     {
