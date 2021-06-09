9088a37adfca3158e0b87a693fc8ae423a7f2f6a
Alex Dryden <adryden3@illinois.edu>
2021-06-09 13:11:43 -0400

    execute on load

--- asset/js/user-default-sites.js
+++ asset/js/user-default-sites.js
@@ -11,19 +11,3 @@ window.addEventListener("load", function () {
         }
     });
 });
-
-
-
-
-
-$("#update_default_sites").click(function() {
-    if($(this).is(":checked"))
-    {
-        $("#default_sites_chosen").attr("hidden",true);
-    }
-    else
-    {
-        $("#default_sites_chosen").attr("hidden",false);
-
-    }
-});
