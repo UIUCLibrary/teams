05013b5cbc4896206bad803270d6320b94d18250
Alex Dryden <adryden3@illinois.edu>
2021-06-29 19:03:41 -0400

    pass the entity manager to allow filtering by team sites

--- src/Service/Form/Element/AllSiteSelectOrderedFactory.php
+++ src/Service/Form/Element/AllSiteSelectOrderedFactory.php
@@ -8,9 +8,20 @@ use Interop\Container\ContainerInterface;
 use Omeka\Service\Form\Element\SiteSelectFactory;
 use Teams\Form\Element\AllItemSetSelect;
 use Teams\Form\Element\AllSiteSelect;
+use Teams\Form\Element\AllSiteSelectOrdered;
+use Teams\Form\Element\AllSiteSelectOrderedByTeam;
 
 class AllSiteSelectOrderedFactory extends SiteSelectFactory
 {
+
+    public function __invoke(ContainerInterface $services, $requestedName, array $options = null)
+    {
+        $element = new AllSiteSelectOrdered(null, $options);
+        $element->setApiManager($services->get('Omeka\ApiManager'));
+        $element->setEntityManager($services->get('Omeka\EntityManager'));
+        return $element;
+    }
+
 
 
 }
