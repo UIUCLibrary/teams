c2afd2d9636b08e7cf4636b5bc723d7b02813cd0
Alex Dryden <adryden3@illinois.edu>
2021-07-01 15:59:59 -0400

    add factory for form element

--- src/Service/Form/Element/BlankTeamSelectFactory.php
+++ src/Service/Form/Element/BlankTeamSelectFactory.php
@@ -4,7 +4,17 @@
 namespace Teams\Service\Form\Element;
 
 
-class BlankTeamSelectFactory
-{
+use Interop\Container\ContainerInterface;
+use Teams\Form\Element\BlankTeamSelect;
 
+class BlankTeamSelectFactory extends AllTeamSelectFactory
+{
+    public function __invoke(ContainerInterface $services, $requestedName, array $options = null)
+    {
+        $element = new BlankTeamSelect(null, $options);
+        $element->setApiManager($services->get('Omeka\ApiManager'));
+        $element->setUrlHelper($services->get('ViewHelperManager')->get('Url'));
+        $element->setEntityManager($services->get('Omeka\EntityManager'));
+        return $element;
+    }
 }
