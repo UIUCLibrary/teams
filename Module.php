82c862b39233b25199cbf979d4dc8eb6ada9796e
Alex Dryden <adryden3@illinois.edu>
2022-07-06 14:59:30 -0400

    Removing this in favor of keeping user default sites up to date with the user's current team.
    Incidentally, this was causing the corrrect sites to not be added
    to items created through batch create because it wasn't checking
    to see if 'team' was included in the request.

--- Module.php
+++ Module.php
@@ -1800,37 +1800,7 @@ SQL;
         echo $view->partial('teams/partial/resource-template/add', ['team_id' => $team_id]);
     }
 
-    //Handle Items
-    //TODO: NEED TO ADD BUTTON TO USE TEAMS FOR SITES (or not, operation is VERY disordered if they arne't in sync)
-    //TODO: NEED TO ADD SOME TEXT OR FIND ANOTHER SOLUTION FOR THE ITEM SITES THAT DONT BELONG TO THE USERS CURRENT TEAM
-    //if user selects to use teams for item sites, update sites data from TeamSite before API executes on form data
-    public function itemPre(Event $event)
-    {
 
-        //get request content
-        $request = $event->getParam('request');
-        $content = $request->getContent();
-
-        //get team(s)
-        $teams = $content['team'];
-
-        $em = $this->getServiceLocator()->get('Omeka\EntityManager');
-
-        //get sites associated with teams(s)
-        $site_ids = [];
-        foreach ($teams as $team_id):
-            $team = $em->getRepository('Teams\Entity\Team')->findOneBy(['id'=>$team_id]);
-        $team_sites = $team->getTeamSites();
-        foreach ($team_sites as $team_site):
-                $site_ids[] = $team_site->getSite()->getId();
-        endforeach;
-        endforeach;
-
-        //update request content
-        $content['o:site'] = $site_ids;
-        $request->setContent($content);
-        $event->setParam('request', $request);
-    }
     /**
      *
      * On update, remove all TeamResources associated with item and associated media, and generate new TeamResources
@@ -2574,12 +2544,6 @@ SQL;
             ItemAdapter::class,
             'api.hydrate.post',
             [$this, 'itemUpdate']
-        );
-
-        $sharedEventManager->attach(
-            ItemAdapter::class,
-            'api.hydrate.pre',
-            [$this, 'itemPre']
         );
 
 
