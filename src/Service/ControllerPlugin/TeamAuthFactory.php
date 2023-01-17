eca4d9ce1f4de55adfd0bd51fcc0bb9e9af11d06
Alex Dryden <adryden3@illinois.edu>
2023-01-17 17:02:31 -0500

    Add user to factory

--- src/Service/ControllerPlugin/TeamAuthFactory.php
+++ src/Service/ControllerPlugin/TeamAuthFactory.php
@@ -4,6 +4,7 @@ namespace Teams\Service\ControllerPlugin;
 use Interop\Container\ContainerInterface;
 use Teams\Mvc\Controller\Plugin\TeamAuth;
 use Laminas\ServiceManager\Factory\FactoryInterface;
+use \Omeka\Entity\User;
 
 class TeamAuthFactory implements FactoryInterface
 {
@@ -16,6 +17,7 @@ class TeamAuthFactory implements FactoryInterface
      */
     public function __invoke(ContainerInterface $services, $requestedName, array $options = null)
     {
-        return new TeamAuth($services->get('Omeka\EntityManager'));
+        $user = new User();
+        return new TeamAuth($services->get('Omeka\EntityManager'), $user);
     }
 }
