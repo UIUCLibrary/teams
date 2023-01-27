ff12fcb068602e396a79e5f390d102b30c4173a4
Alex Dryden <adryden3@illinois.edu>
2023-01-26 19:02:17 -0500

    Add basic lookup features Adjust terms used because TeamResources use a composite primary key instead of an incremented ID. It was also necessary to turn off strict_trans_tables to avoid getting error: "Expression #2 of SELECT list is not in GROUP BY clause and c ontains nonaggregated column 'omeka_local.t0_.resource_id' which is not functionally dependent on columns in GROUP BY clause; this is incompatible with sql_mode=only_full_group_by". This may require further attention.

--- src/Api/Adapter/TeamResourceAdapter.php
+++ src/Api/Adapter/TeamResourceAdapter.php
@@ -4,8 +4,11 @@
 namespace Teams\Api\Adapter;
 
 use Doctrine\ORM\QueryBuilder;
+use Doctrine\ORM\Tools\Pagination\Paginator;
+use Laminas\EventManager\Event;
 use Omeka\Api\Adapter\AbstractEntityAdapter;
 use Omeka\Api\Request;
+use Omeka\Api\Response;
 use Omeka\Entity\EntityInterface;
 use Omeka\Stdlib\ErrorStore;
 use Teams\Api\Representation\TeamResourceRepresentation;
@@ -67,4 +70,137 @@ class TeamResourceAdapter extends AbstractEntityAdapter
         }
 
     }
+
+    public function search(Request $request)
+    {
+        $query = $request->getContent();
+
+        // Set default query parameters
+        if (!isset($query['page'])) {
+            $query['page'] = null;
+        }
+        if (!isset($query['per_page'])) {
+            $query['per_page'] = null;
+        }
+        if (!isset($query['limit'])) {
+            $query['limit'] = null;
+        }
+        if (!isset($query['offset'])) {
+            $query['offset'] = null;
+        }
+        if (!isset($query['sort_by'])) {
+            $query['sort_by'] = null;
+        }
+        if (isset($query['sort_order'])
+            && in_array(strtoupper($query['sort_order']), ['ASC', 'DESC'])
+        ) {
+            $query['sort_order'] = strtoupper($query['sort_order']);
+        } else {
+            $query['sort_order'] = 'ASC';
+        }
+        if (!isset($query['return_scalar'])) {
+            $query['return_scalar'] = null;
+        }
+
+        // Begin building the search query.
+        $entityClass = $this->getEntityClass();
+
+        $this->index = 0;
+        $qb = $this->getEntityManager()
+            ->createQueryBuilder()
+            ->select('omeka_root')
+            ->from($entityClass, 'omeka_root');
+        $this->buildBaseQuery($qb, $query);
+        $this->buildQuery($qb, $query);
+        $qb->groupBy("omeka_root.team");
+
+        // Trigger the search.query event.
+        $event = new Event('api.search.query', $this, [
+            'queryBuilder' => $qb,
+            'request' => $request,
+        ]);
+        $this->getEventManager()->triggerEvent($event);
+
+        // Add the LIMIT clause.
+        $this->limitQuery($qb, $query);
+
+        // Before adding the ORDER BY clause, set a paginator responsible for
+        // getting the total count. This optimization excludes the ORDER BY
+        // clause from the count query, greatly speeding up response time.
+        $countQb = clone $qb;
+        $countQb->select('1')->resetDQLPart('orderBy');
+        $countPaginator = new Paginator($countQb, false);
+
+        // Add the ORDER BY clause. Always sort by entity ID in addition to any
+        // sorting the adapters add.
+        $this->sortQuery($qb, $query);
+        $qb->addOrderBy("omeka_root.team", $query['sort_order']);
+
+
+        $paginator = new Paginator($qb, false);
+        $entities = [];
+        // Don't make the request if the LIMIT is set to zero. Useful if the
+        // only information needed is total results.
+        if ($qb->getMaxResults() || null === $qb->getMaxResults()) {
+            foreach ($paginator as $entity) {
+                if (is_array($entity)) {
+                    // Remove non-entity columns added to the SELECT. You can use
+                    // "AS HIDDEN {alias}" to avoid this condition.
+                    $entity = $entity[0];
+                }
+                $entities[] = $entity;
+            }
+        }
+
+        $response = new Response($entities);
+        $response->setTotalResults($countPaginator->count());
+        return $response;
+    }
+
+    public function findEntity($criteria, $request = null)
+    {
+        if (!is_array($criteria)) {
+            $criteria = ['id' => $criteria];
+        }
+
+        $entityClass = $this->getEntityClass();
+        $this->index = 0;
+        $qb = $this->getEntityManager()->createQueryBuilder();
+        $qb->select('omeka_root')->from($entityClass, 'omeka_root');
+        foreach ($criteria as $field => $value) {
+            $qb->andWhere($qb->expr()->eq(
+                "omeka_root.$field",
+                $this->createNamedParameter($qb, $value)
+            ));
+        }
+        $qb->setMaxResults(1);
+
+        $event = new Event('api.find.query', $this, [
+            'queryBuilder' => $qb,
+            'request' => $request,
+        ]);
+
+        $this->getEventManager()->triggerEvent($event);
+        $entity = $qb->getQuery()->getOneOrNullResult();
+        if (!$entity) {
+            throw new Exception\NotFoundException(sprintf(
+                $this->getTranslator()->translate('%1$s entity with criteria %2$s not found'),
+                $entityClass, json_encode($criteria)
+            ));
+        }
+        return $entity;
+    }
+
+    public function read(Request $request)
+    {
+        $entity = $this->findEntity(['team'=>1, 'resource'=>1074], $request);
+        $this->authorize($entity, Request::READ);
+        $event = new Event('api.find.post', $this, [
+            'entity' => $entity,
+            'request' => $request,
+        ]);
+        $this->getEventManager()->triggerEvent($event);
+        return new Response($entity);
+    }
+
 }
