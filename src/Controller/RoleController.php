038b595762100c5cc336a001337ee9220b2bd1cf
Alex Dryden <adryden3@illinois.edu>
2023-03-28 12:41:00 -0400

    Add stubs to normalize architecture

---
+++ src/Controller/RoleController.php
@@ -0,0 +1,33 @@
+<?php
+namespace Teams\Controller;
+
+use Laminas\Mvc\Controller\AbstractActionController;
+
+
+class RoleController extends AbstractActionController
+{
+    public function addAction()
+    {
+
+    }
+
+    public function deleteAction()
+    {
+
+    }
+
+    public function editAction()
+    {
+
+    }
+
+    public function showAction()
+    {
+
+    }
+
+    public function browseAction()
+    {
+
+    }
+}
