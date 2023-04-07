f555a48fcf028f4c6ce6b16de4ee6fc9a46b8ead
Alex Dryden <adryden3@illinois.edu>
2023-04-07 16:56:11 -0400

    Add a form element to bypass team filtering

---
+++ view/teams/common/sort-selector-bypass-teams.php
@@ -0,0 +1,12 @@
+<?php
+$translate = $this->plugin('translate');
+$escape = $this->plugin('escapeHtml');
+?>
+<form class="sorting" action="">
+    <?php echo $this->queryToHiddenInputs(['bypass_team_filter']); ?>
+    <select name="bypass_team_filter">
+        <option value="">Filter results by current team</option>
+        <option <?php if($bypassTeams):?> selected <?php endif;?> value="true">Bypass teams filter</option>
+    </select>
+    <button type="submit"><?php echo $translate('Sort'); ?></button>
+</form>
