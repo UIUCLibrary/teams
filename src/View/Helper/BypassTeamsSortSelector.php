a9346b0fbf2e3258d32812c0e6479b60f36cea9d
Alex Dryden <adryden3@illinois.edu>
2023-04-07 19:26:37 -0400

    View helper to insert the team filter toggle

--- src/View/Helper/BypassTeamsSortSelector.php
+++ src/View/Helper/BypassTeamsSortSelector.php
@@ -2,7 +2,35 @@
 
 namespace Teams\View\Helper;
 
-class BypassTeamsSortSelector
+use Laminas\View\Helper\AbstractHelper;
+
+class BypassTeamsSortSelector extends AbstractHelper
 {
+    /**
+     * The default partial view script.
+     */
+    const PARTIAL_NAME = 'teams/common/sort-selector-bypass-teams';
+
+
+    /**
+     * Modified from core SortSelector View Helper
+     *
+     * @param string|null $partialName Name of view script, or a view model
+     * @return string
+     */
+    public function __invoke(string $partialName = null)
+    {
+        $partialName = $partialName ?: self::PARTIAL_NAME;
+
+        $view = $this->getView();
+        $params = $view->params();
+        $bypassTeams = $params->fromQuery('bypass_team_filter');
+
+        $args = [
+            'bypassTeams' => $bypassTeams,
+        ];
+
+        return $view->partial($partialName, $args);
+    }
 
 }
