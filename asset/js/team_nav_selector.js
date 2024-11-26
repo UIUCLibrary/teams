cfc38fb43a02e2723da9adfeb6d342c24b5ad9bb
Alex Dryden <adryden3@illinois.edu>
2024-11-26 09:44:06 -0500

    Update admin menu for 4.1 changes

--- asset/js/team_nav_selector.js
+++ asset/js/team_nav_selector.js
@@ -1,18 +1,25 @@
 window.addEventListener("load", function () {
-    let newP = document.createElement('p');
-    let newAnchor = document.createElement('a');
+    let team = document.createElement('p');
+    let viewTeam = document.createElement('a');
+    let editTeam = document.createElement('a');
+    let editTeamSR = document.createElement('span')
 
-    newAnchor.setAttribute('href', teamHref);
-    newAnchor.innerText = teamName;
+    team.setAttribute('class','user-id');
+    viewTeam.setAttribute('class','o-icon-users button user-show');
+    viewTeam.setAttribute('href', editTeamHref);
+    viewTeam.innerText = teamName;
+    editTeam.setAttribute('class', 'user-settings button')
+    editTeamSR.setAttribute('class', 'sr-only')
+    editTeamSR.innerText = "Edit Team"
 
-    newP.innerText = 'Current Team';
-    newP.setAttribute('class','user-id');
-    newP.setAttribute('id', 'nav_team');
-    newAnchor.setAttribute('id', 'team-link');
+    team.setAttribute('id', 'nav_team');
+    viewTeam.setAttribute('id', 'team-link');
 
-    newP.appendChild(newAnchor);
+    editTeam.appendChild(editTeamSR)
+    team.appendChild(viewTeam);
+    team.appendChild(editTeam)
 
-    document.getElementById("user").appendChild(newP);
+    document.getElementById("user").appendChild(team);
     let newButton = document.createElement('p');
     newButton.className = 'logout';
     let buttonAnchor = document.createElement('a');
