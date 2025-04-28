f3bcd319f1b7f94c21a6eae0cfbf6f12ce18b98f
Alex Dryden <adryden3@illinois.edu>
2025-04-28 13:10:31 -0400

    comment: add docstring

--- Module.php
+++ Module.php
@@ -2473,7 +2473,11 @@ SQL;
 
     //Add Team options to Batch Edit
 
-
+    /**
+     * Add  "Add Team" and "Remove Team" select elements to the batch edit forme
+     * @param Event $event
+     * @return void
+     */
     public function addTeamToBatchEditForm(Event $event) {
         $form = $event->getTarget();
 
