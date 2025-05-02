5284f92f2af7d09d5e25eecef864b666194cc351
Alex Dryden <adryden3@illinois.edu>
2025-05-02 10:59:27 -0400

    fix: use correct variable name

--- Module.php
+++ Module.php
@@ -1897,7 +1897,7 @@ SQL;
                     $add = $request->getContent()['add_team'];
                 }
             } else {
-                $remove = [];
+                $add = [];
             }
             if (array_key_exists('remove_team', $request->getContent()) ||
                 array_key_exists('add_team', $request->getContent())) {
