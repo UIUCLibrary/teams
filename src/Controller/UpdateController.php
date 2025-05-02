2760c289c37946e5f13be5193665d80329d71b7e
Alex Dryden <adryden3@illinois.edu>
2025-05-02 10:11:07 -0400

    fix: make search query bypass teams filter
    so that admins can add any resources to a team and not just the
    resources from their current team.

--- src/Controller/UpdateController.php
+++ src/Controller/UpdateController.php
@@ -495,6 +495,7 @@ class UpdateController extends AbstractActionController
             $this->messenger()->addError("You aren't authorized to change this team");
             return $view;
         } else {
+            $post_data['item_assignment_action'] .= '&bypass_team_filter=true';
             if ($post_data['item_assignment_action'] && $post_data['item_assignment_action'] !== 'no_action') {
                 $this->jobDispatcher()->dispatch('Teams\Job\UpdateTeamResources', [
                     'teams' => [$team_id => $post_data['item_pool']],
