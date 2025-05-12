5175caeb981115ab9e5f2495edae2ac7d9ac512b
Alex Dryden <adryden3@illinois.edu>
2025-05-12 09:50:51 -0400

    feat: automatically add global admin to bypass filter on upgrade
    Before v4.1 global admins were the only user who could use the
    bypass teams filter feature. In 4.1, that feature can optionally
    be tuned to accomodate any level of user role in the module config.
    As such, to keep behavior consistent when upgrading, we need to add
    the global admin to the settting.

--- Module.php
+++ Module.php
@@ -180,6 +180,11 @@ SQL;
             $conn = $serviceLocator->get('Omeka\Connection');
             $conn->exec('ALTER TABLE team_user MODIFY id INT NOT NULL AUTO_INCREMENT');
         }
+        if (version_compare($oldVersion,'4.0.1', '<')) {
+            //add global admin to the list of settings for bypass team users
+            $globalSettings = $this->getServiceLocator()->get('Omeka\Settings');
+            $globalSettings->set('teams_filter_bypass_roles', 'global_adin');
+        }
     }
 
     public function updateAllUserSites()
